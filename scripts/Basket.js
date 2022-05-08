216457f63ef9b2607bf34258a1ca439f223fc1e5
your github user nameShubham0442 <your github emailshubhamrakhonde507@gmail.com>
2022-05-08 15:19:53 +0530

    basket added

--- scripts/Basket.js
+++ scripts/Basket.js
@@ -1,6 +1,8 @@
 
+// getting the key from local storage
 let basketData = JSON.parse(localStorage.getItem("basket"))
 
+//calculating the number of products inside basket
 let No_of_products = basketData.length 
 
 console.log(No_of_products)
@@ -9,11 +11,14 @@ document.querySelector("#basket-length>p").textContent = `${basketData.length} P
 
 
 
+// mapping the data inside the basket 
+
+
 let displayBasket = (basketData) =>{
 
     document.getElementById("left").innerHTML = null
 
-    basketData.map(({image, name, price})=>{
+    basketData.map(({image, name, price},ind)=>{
          
         let outerBox1 = document.createElement("div")
         outerBox1.setAttribute("id","outerBox1") 
@@ -64,10 +69,6 @@ let displayBasket = (basketData) =>{
 
         let horLine = document.createElement("hr") 
         
-
-
-
-
         let Qty = document.createElement("select") 
         let Qty1 = document.createElement("option")
         Qty1.innerText = "Qty1"
@@ -78,6 +79,20 @@ let displayBasket = (basketData) =>{
         let removeBtn = document.createElement("button") 
         removeBtn.innerText = "Remove" 
 
+        let data1 = {
+            image,
+            name,
+            price
+        }
+
+
+        removeBtn.addEventListener("click", function (){
+            removeFromBasket(data1,ind)  
+
+        })
+
+
+
         let favBtn = document.createElement("button") 
         favBtn.innerText = "Move to favourites"
         
@@ -85,14 +100,82 @@ let displayBasket = (basketData) =>{
         verLine.innerText = "|" 
 
         innerbox3.append(removeBtn, verLine, favBtn) 
-
-        
+       
         outerBox1.append(innerbox1, innerbox2, horLine, innerbox3) 
 
         document.getElementById("left").append(outerBox1)
+
+       
     })
 }
 
 
 
-displayBasket(basketData)
+displayBasket(basketData) 
+
+// function for removing the items from basket
+function removeFromBasket(elem,index){
+                basketData.splice(index,1)
+                localStorage.setItem("basket",JSON.stringify(basketData))
+                window.location.reload()
+    }
+
+
+// function created for checking pincode
+let checkPincode = () =>{
+    let pincode1 = document.querySelector("#pincode").value 
+    let outputPin = document.querySelector("#checkedOp")
+
+    if(pincode1 === "")
+    {
+        outputPin.innerText = "Please enter correct Pincode"
+        outputPin.style.color = "red"
+    }
+    else if(pincode1.length===6)
+    {
+        outputPin.innerText = "Delivery is Available"
+        outputPin.style.color = "green"
+    }
+    else if(pincode1.length>6 || pincode1.length<6)
+    {
+        outputPin.innerText = "Please enter correct Pincode"
+        outputPin.style.color = "red"
+    }
+} 
+
+
+//calculating the total price of all products 
+
+let total = basketData.reduce(function (sum,elem){
+    return sum + Number(elem.price);
+},0)
+console.log(total)
+
+let ApplyPromoCode = () =>{
+
+    let inputPromp = document.querySelector("#inputPromp").value 
+    
+    if(inputPromp === "MASAI10")
+    {
+        let cutoffPrice = (total * 10/100)
+
+        total = total - cutoffPrice
+    
+        mrp.innerText = `₹ ${total}` 
+        totalPrice.innerText = `₹ ${total}`
+        
+    } 
+}
+
+let mrp = document.querySelector("#MRPprice>p+p")
+mrp.innerText = `₹ ${total}` 
+
+let totalPrice = document.querySelector("#totalDiv>div>p+p")
+totalPrice.innerText = `₹ ${total}` 
+
+
+
+
+
+
+
